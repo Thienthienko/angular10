15aa4c13f61a95ce78bc8e2f85dd17e2ec9345b3
Thien Nguyen <thien.ngu25@gmail.com>
2024-11-14 14:17:56 +0100

    add api service

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -4,6 +4,7 @@ import { Component, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Article } from '../../models/article.model';
 import { ArticleThumbnailComponent } from '../article-thumbnail/article-thumbnail.component';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-article-list',
@@ -15,16 +16,27 @@ import { ArticleThumbnailComponent } from '../article-thumbnail/article-thumbnai
 export class ArticleListComponent {
   articles$!: Observable<Article[]>;
 
+  private apiService = inject(ApiService);
+
   http = inject(HttpClient);
 
   ngOnInit() {
-    this.getArticles();
+    this.articles$ = this.apiService.getArticles();
   }
 
-  getArticles() {
-    this.articles$ = this.http.get<Article[]>('http://localhost:3000/articles');
-  }
   handleLike(article: Article) {
     article.isLiked = !article.isLiked;
   }
+
+  // articles$!: Observable<Article[]>;
+
+  // http = inject(HttpClient);
+
+  // ngOnInit() {
+  //   this.getArticles();
+  // }
+
+  // getArticles() {
+  //   this.articles$ = this.http.get<Article[]>('http://localhost:3000/articles');
+  // }
 }
